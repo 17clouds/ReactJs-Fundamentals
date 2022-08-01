f23979b2a0e0bdc31b012537519c1128b4a1df67
17clouds <92887957+17clouds@users.noreply.github.com>
2022-08-01 22:57:59 +0300

    feat: get posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-
-import React, {useRef, useState} from "react";
+// eslint-disable-next-line
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import'./styles/app.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
+
 function App() {
 
-    const [posts,setPosts] = useState([
-        {id:1, title: 'JavaScript 1', body: 'Description'},
-        {id:2, title: 'JavaScript 2', body: 'Description'},
-        {id:3, title: 'JavaScript 3', body: 'Description'},
-    ])
+    const [posts,setPosts] = useState([])
+    const [filter, setFilter] = useState({sort:'', query: ''});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPosts()
+    },[])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
+    }
+    
+    async function fetchPosts() {
+        setIsPostsLoading(true);
+        setTimeout(async () => {
+            const posts = await PostService.getAll();
+            setPosts(posts)
+            setIsPostsLoading(false);
+        }, 2000)
+
     }
 
     const removePost = (post) => {
@@ -24,18 +44,26 @@ function App() {
 
     return (
     <div className="App">
+        <MyButton style={{marginRight:10}} onClick={fetchPosts}>GET POSTS</MyButton>
+        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+            Create Post
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
 
-        <PostForm create={createPost}/>
-        {posts.length
-            ?
-            <PostList remove={removePost} posts={posts} title={'JS List'}/>
-            :
-            <h2 style={{textAlign: 'center'}}>
-                Posts not found
-            </h2>
+
+        <hr style={{margin: '15px 0'}}/>
+
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+
+        {isPostsLoading
+            ?<div style={{display: 'flex', justifyContent: 'center',marginTop: 50}}><Loader/></div>
+            :<PostList remove={removePost} posts={sortedAndSearchedPosts} title={'JS List'}/>
         }
-
-
     </div>
   );
 }
