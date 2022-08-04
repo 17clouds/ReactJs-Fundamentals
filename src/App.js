305a4a27694aaef36f68c438a07bc4a678845a27
17clouds <92887957+17clouds@users.noreply.github.com>
2022-08-04 12:00:44 +0300

    feat:react router and some else function

--- src/App.js
+++ src/App.js
@@ -1,73 +1,21 @@
-// eslint-disable-next-line
-import React, {useEffect, useMemo, useRef, useState} from "react";
-import'./styles/app.css'
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/modal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/loader/Loader";
-import {useFetching} from "./hooks/useFetching";
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles/app.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+
 
 function App() {
-
-    const [posts,setPosts] = useState([])
-    const [filter, setFilter] = useState({sort:'', query: ''});
-    const [modal, setModal] = useState(false);
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-    const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
-        const posts = await PostService.getAll();
-        setPosts(posts)
-    })
-
-    useEffect(() => {
-        fetchPosts()
-    },[])
-
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-        setModal(false)
-    }
-
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id))
-    }
-
     return (
-    <div className="App">
-
-        <MyButton style={{marginRight:10}} onClick={fetchPosts}>GET POSTS</MyButton>
-        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-            Create Post
-        </MyButton>
-        <MyModal visible={modal} setVisible={setModal}>
-            <PostForm create={createPost}/>
-        </MyModal>
-
-        <hr style={{margin: '15px 0'}}/>
-
-        <PostFilter
-            filter={filter}
-            setFilter={setFilter}
-        />
-
-        {postError &&
-            <h1>Error ${postError}</h1>
-        }
-
-        {isPostsLoading
-            ?<div style={{display: 'flex', justifyContent: 'center',marginTop: 50}}><Loader/></div>
-            :<PostList remove={removePost} posts={sortedAndSearchedPosts} title={'JS List'}/>
-        }
-
-    </div>
-  );
+        // <div>It works</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/about" element={<About/>} />
+                <Route path="/posts" element={<Posts />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
-
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>This project was created for education purposes</div>
+    );
+};
+
+export default About;
